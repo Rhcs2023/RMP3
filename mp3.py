be7e61d079c933fecb442b0778b05a9c0b5275ff
Space <143919787+Rhcs2023@users.noreply.github.com>
2025-02-07 22:37:12 -0600

    Create mp3.py

---
+++ mp3.py
@@ -0,0 +1,57 @@
+import streamlit as st
+from gtts import gTTS
+import tempfile
+
+# Diccionario con palabras a traducir
+diccionario = {
+    # Agrega aquí tus traducciones
+    # 'a': 'uno ',
+    # 'b': 'dos ',
+    # 'c': 'tres ',
+    # 'd': 'cuatro ',
+    # 'e': 'cinco ',
+    # 'f': 'seis ',
+    # 'g': 'siete ',
+    # 'mi': 'ri ',
+    # 'tuyo': 'tu ',
+    # 'mío': 'mi ',
+}
+
+def traducir_oracion(oracion):
+    palabras = oracion.split()
+    oracion_traducida = " ".join([diccionario.get(palabra.lower(), palabra) for palabra in palabras])
+    return oracion_traducida
+
+def reproducir_audio(texto, lang):
+    tts = gTTS(text=texto, lang=lang, slow=False)
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3') as tmp:
+        tts.save(tmp.name)
+        return tmp.name  # Retorna la ruta del archivo temporal
+
+st.title("Texto a Voz")
+
+# Estado de la sesión para la traducción
+if 'oracion_traducida' not in st.session_state:
+    st.session_state.oracion_traducida = ""
+
+# Opción para introducir texto
+oracion_usuario = st.text_area("Introduce una oración :", height=200)
+
+# Botón para ejecutar la traducción
+if st.button("ir"):
+    if oracion_usuario:
+        oracion_traducida = traducir_oracion(oracion_usuario)
+        st.session_state.oracion_traducida = oracion_traducida
+        audio_file_path = reproducir_audio(oracion_traducida, 'es')  # Usando español por defecto
+        st.audio(audio_file_path, format='audio/mp3')
+
+        # Botón para descargar el archivo de audio
+        with open(audio_file_path, 'rb') as audio_file:
+            st.download_button(
+                label="Descargar audio en MP3",
+                data=audio_file,
+                file_name="audio.mp3",
+                mime="audio/mp3"
+            )
+    else:
+        st.warning("Por favor, introduce una oración antes de traducir.")
